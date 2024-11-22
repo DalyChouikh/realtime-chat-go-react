3302290f270aba617b687647ab38e787671d5598
DalyChouikh <chouikhdaly215@gmail.com>
2024-11-22 17:52:27 +0100

    Imported the Header and ChatHistory components and added chatHistory state to pass it to the ChatHistory Component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { connect, sendMsg } from "./api";
+import Header from "./components/Header";
+import ChatHistory from "./components/ChatHistory";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [chatHistory, setChatHistory] = useState<string[]>([]);
+  connect((msg: MessageEvent) => {
+    console.log("New message");
+    setChatHistory((chatHistory: string[]) => [...chatHistory, msg.data]);
+    console.log(chatHistory);
+  });
+  const sendMessage = () => {
+    console.log("sending message: Hello");
+    sendMsg("Hello");
+  };
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="container">
+      <Header />
+      <ChatHistory messages={chatHistory} />
+      <button onClick={sendMessage}>Send</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
