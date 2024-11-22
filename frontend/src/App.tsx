97b7ea8749d36927a875b36823497e0c6b489435
DalyChouikh <chouikhdaly215@gmail.com>
2024-11-22 22:30:29 +0100

    Added sendMessageOnKeyPress and sendMessageOnButtonClick to send the desired message in the input

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import { connect, sendMsg } from "./api";
 import Header from "./components/Header";
 import ChatHistory from "./components/ChatHistory";
+import ChatInput from "./components/ChatInput";
+import SendButton from "./components/SendButton";
 
 function App() {
   const [chatHistory, setChatHistory] = useState<string[]>([]);
@@ -13,15 +15,25 @@ function App() {
       console.log(chatHistory);
     });
   }, []);
-  const sendMessage = () => {
-    console.log("sending message: Hello");
-    sendMsg("Hello");
+  const sendMessageOnKeyPress = (event: React.KeyboardEvent) => {
+    if (event.key === "Enter") {
+      const input = event.target as HTMLInputElement;
+      sendMsg(input.value);
+      input.value = "";
+    }
+  };
+
+  const sendMessageOnButtonClick = () => {
+    const input = document.querySelector(".chat-input") as HTMLInputElement;
+    sendMsg(input.value);
+    input.value = "";
   };
   return (
     <div className="container">
       <Header />
       <ChatHistory messages={chatHistory} />
-      <button onClick={sendMessage}>Send</button>
+      <ChatInput onSendMessage={sendMessageOnKeyPress} />
+      <SendButton onClick={sendMessageOnButtonClick} />
     </div>
   );
 }
